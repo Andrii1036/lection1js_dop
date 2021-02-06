93dc003e739ed0da7f9befa2c873956e55c424e0
Andrii1036 <kovalic1036@gmail.com>
2021-02-06 14:31:53 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log('...............Завдання ДОП-1...........');
+// Посмотрев на следюующие выражения, скажите, каков будет вывод в консоль
+// console.log(132 > 100 && 45 < 12 );
+// console.log(34 > 33 && 23 < 90 );
+// console.log(99 > 100 && 45 > 12 );
+// console.log(132 > 100 || 45 < 12 );
+// console.log(111 > 11 || 45 < 111 );
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );
+// console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));
+// console.log(!!'-1');
+// console.log(!!-1);
+// console.log(!!'0');
+// console.log(!!'null');
+// console.log(!!'undefined');
+// console.log(!!(3/'owu'));
+// console.log((111 > 11 || 45 < 111) ||  !!'0');
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));
+
+console.log('(132 > 100 && 45 < 12)- false');
+console.log('(34 > 33 && 23 < 90 )-true');
+console.log('(99 > 100 && 45 > 12 )-false');
+console.log('(132 > 100 || 45 < 12 )-true');
+console.log('(111 > 11 || 45 < 111 )-true');
+console.log('((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) )- true');
+console.log('((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ) - true');
+console.log('((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ) - false');
+console.log('((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )) - true');
+console.log('(!!"-1") - true');
+console.log('(!!-1) - true');
+console.log('(!!"0") - true');
+console.log('(!!"null") - true');
+console.log('(!!"undefined") - true');
+console.log('(!!(3/"owu")) - false');
+console.log('((111 > 11 || 45 < 111) ||  !!"0") - true');
+console.log('((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 )) - false');
